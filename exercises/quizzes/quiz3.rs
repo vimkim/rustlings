03044ec06b60828e4569a0d9d8d39dfab160538f
Daehyun Kim <kimdhyungg@gmail.com>
2025-08-02 05:58:58 +0900

    progress: traits

--- exercises/quizzes/quiz3.rs
+++ exercises/quizzes/quiz3.rs
@@ -11,19 +11,35 @@
 // Make the necessary code changes in the struct `ReportCard` and the impl
 // block to support alphabetical report cards in addition to numerical ones.
 
+trait Grade {
+    fn print_grade(&self) -> String;
+}
+
+impl Grade for f32 {
+    fn print_grade(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl Grade for &str {
+    fn print_grade(&self) -> String {
+        self.to_string()
+    }
+}
+
 // TODO: Adjust the struct as described above.
-struct ReportCard {
-    grade: f32,
+struct ReportCard<T: Grade> {
+    grade: T,
     student_name: String,
     student_age: u8,
 }
 
 // TODO: Adjust the impl block as described above.
-impl ReportCard {
-    fn print(&self) -> String {
+impl<T: Grade> ReportCard<T> {
+    fn print(self) -> String {
         format!(
             "{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade,
+            &self.student_name, &self.student_age, &self.grade.print_grade()
         )
     }
 }
